ddb8eea231887abdae666daf08b29f266e711314
Deja Jackson <deja.jackson@dejajackson.me>
2021-07-21 15:28:55 -0700

    Initial Commit

---
+++ p1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  p1
+//
+//  Created by Deja Jackson on 7/21/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
